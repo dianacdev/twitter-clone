1d540a073a15b7c695fd1545402c55f0c540d126
Diana C <diana@gmetrix.com>
2023-03-29 21:23:21 -0600

    Added Login Modal!

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from './layout/Sidebar';
+import FollowBar from './layout/FollowBar';
 
 interface LayoutProps{
     children: React.ReactNode;
@@ -7,13 +8,14 @@ interface LayoutProps{
 
 const Layout:React.FC<LayoutProps> = ({children}) => {
   return (
-    <div className='h-screen bg-slate-950'>
+    <div className='h-screen bg-neutral-950'>
         <div className='container h-full mx-auto xl:px-30 max-w-6xl'>
             <div className='grid grid-cols-4 h-full'>
                 <Sidebar/>
                 <div className='col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800'>
                     {children}
                 </div>
+                <FollowBar />
             </div>
         </div>
     </div>
